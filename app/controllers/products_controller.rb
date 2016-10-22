327b7236913784e6673a47f39c8cb436a3ee771c
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-10-22 11:15:00 -0500

    adjustements in edit and create, partial(layout)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
+
+  before_action :find_product, only: [:edit, :show, :update, :destroy]
+
+  
   def new
     @product = Product.new
   end
@@ -7,7 +11,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = 'The product have been saved'
-      redirect_to @product
+      redirect_to product_path(id: @product.id , it_was: 'created')
     else
       @erros = @product.errors
       render :new
@@ -17,20 +21,37 @@ class ProductsController < ApplicationController
   def edit
   end
 
+  def update
+    if @product.update(product_params)
+       flash[:notice] = 'Product was sucessfully updated'
+       redirect_to product_path(id: @product.id , it_was: 'updated')
+    else
+      render :edit
+    end
+  end
+
   def index
     @products = Product.all
   end
 
   def destroy
+    product.destroy
+    flash[:notice] = 'Product was sucessfully destroyed'
+    redirect_to products_path
+
   end
 
   def show
-    @product = Product.find(params[:id])
+    @action = params[:it_was]
   end
 
   private
   #Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description)
+    end
+
+    def find_product
+      @product = Product.find(params[:id])
     end
 end
