67db16f0906372b7633fdb8c2ca8d671ebac2997
Liliana Restrepo Torres <lilianarestrepot@gmail.com>
2016-10-21 19:40:23 -0500

    Initial commit with new product form

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # get 'products/new'
+  #
+  # get 'products/edit'
+  #
+  # get 'products/destroy'
+  #
+  # get 'products/show'
+  #
+  # get 'products/index'
+
+    resources :products
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
