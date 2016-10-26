a93bf1e74ae18530a96ca20eee7266f9b89ebf9b
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-10-26 18:34:10 -0500

    root pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
-  get 'pages/home'
-
 	resources :categories
     resources :products
+    root 'pages#home'
+    #get 'welcome' => 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
